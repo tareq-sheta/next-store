c75b05745eb0d28e27e669a608ac4818015b6087
tareq <tareq@mail.com>
2026-03-20 03:14:09 +0200

    added products route

---
+++ app/products/page.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+import ProductsPageContent from "./ProductsPageContent";
+
+export default function ProductsPage() {
+  return (
+    <Suspense
+      fallback={
+        <div className="container mx-auto px-4 py-20 text-center text-gray-400">
+          Loading products...
+        </div>
+      }
+    >
+      <ProductsPageContent />
+    </Suspense>
+  );
+}
